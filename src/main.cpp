1856d44cd56c093e24a93210928684c07f4a1ddd
Seliaste <aena.aria@protonmail.com>
2022-10-26 11:10:16 +0200

    deleted build folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #define MAIN_CPP
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello world!\n";
+    SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
 
