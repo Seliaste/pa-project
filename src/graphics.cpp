e0640d5e3f500e765bafff9d00d5c17d7812f507
BigFernand69 <wieczorek.thomas@outlook.fr>
2022-11-09 10:41:55 +0100

    added loading functions to graphics.cpp

---
+++ src/graphics.cpp
@@ -0,0 +1,19 @@
+#include "graphics.h"
+
+void clear_resources(resources_t *resources)
+{
+    delete resources->background;
+    delete resources;
+}
+SDL_Texture* charger_image (const char* nomfichier, SDL_Renderer* renderer){
+    SDL_Surface* surface = SDL_LoadBMP(nomfichier);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,surface);
+    return texture;
+}
+SDL_Texture* charger_image_transparente(const char* nomfichier,SDL_Renderer* renderer,Uint8 r,Uint8 g,Uint8 b){
+        SDL_Surface* surface = SDL_LoadBMP(nomfichier);
+        Uint32 code = SDL_MapRGB(surface->format,r,g,b);
+        SDL_SetColorKey(surface,SDL_TRUE,code);
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,surface);
+        return texture;
+}
