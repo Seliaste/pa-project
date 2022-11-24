eda9877dcee9121e3401288ea89bcc44b30f0add
Seliaste <aena.aria@protonmail.com>
2022-11-24 17:17:54 +0100

    Created a trigger object for checkpoints and finish lines

---
+++ src/Trigger.cpp
@@ -0,0 +1,18 @@
+//
+// Created by seliaste on 24/11/22.
+//
+
+#include "Trigger.h"
+
+Trigger::Trigger(int x, int y, int w, int h) {
+    origin = glm::vec2(x,y);
+    dimension = glm::vec2(w,h);
+}
+
+bool Trigger::is_overlapping(Car *player) const {
+    return
+    player->get_pos_x() > origin.x &&
+    player->get_pos_x() < origin.x + dimension.x &&
+    player->get_pos_y() > origin.y &&
+    player->get_pos_y() < origin.x + dimension.y;
+}
---
+++ src/Trigger.h
@@ -0,0 +1,29 @@
+//
+// Created by seliaste on 24/11/22.
+//
+
+#ifndef PA_PROJECT_TRIGGER_H
+#define PA_PROJECT_TRIGGER_H
+#include <glm/vec2.hpp>
+#include "Car.h"
+
+/**
+ * A trigger will be a class used by the start/finish line and checkpoint to be sure the player has made a valid lap
+ */
+class Trigger {
+private:
+    glm::vec2 origin{};
+    glm::vec2 dimension{};
+public:
+    /**
+     * @param x Horizontal position
+     * @param y Vertical position
+     * @param w Width
+     * @param h Height
+     */
+    Trigger(int x, int y, int w, int h);
+    bool is_overlapping(Car* player) const;
+};
+
+
+#endif //PA_PROJECT_TRIGGER_H
